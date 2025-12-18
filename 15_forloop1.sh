247607a939aadb8cab0be75810507e4170aec9cb
uddhav19 <uddhavhon8265@gmail.com>
2025-12-18 22:39:35 +0530

    loops file added

---
+++ 15_forloop1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for i in 1 2 3 4 5 6 7 8 9 10
+do
+	echo "number is $i"
+done
+
+for name in adi swanand avdhut
+do
+	echo "name of students are $name"
+done
+
+for j in {20..25}
+do
+	echo "range is $j"
+
+done
+
