5bba5921ce34fc554cb130af834df84249018ab7
uddhav19 <uddhavhon8265@gmail.com>
2026-02-19 00:01:48 +0530

    new files added

---
+++ new_practice_session/if-else.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+a=4
+b=5
+if [[ $a -gt $b ]]
+then
+	echo "a is greter"
+else
+	echo "b is greater"
+fi
--- new_practice_session/set.sh
+++ new_practice_session/set.sh
@@ -5,9 +5,12 @@
 
 
 set -x # debugg mode
-
+set -e # exit the script when ther is error
+set -o pipefail
 free -g
 
 df -h
 
+#awk command
+ps  -ef | grep name | awk -F" " '{print $2}'
 
